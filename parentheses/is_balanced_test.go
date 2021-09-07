d6d51d19d16b5ad6260712c0eac63de09a93b097
Dyleme <aleksei.dyavgo@gmail.com>
2021-09-07 10:34:14 +0300

    add new tests and rename old ones

--- parentheses/is_balanced_test.go
+++ parentheses/is_balanced_test.go
@@ -14,17 +14,18 @@ func TestIsBalanced(t *testing.T) {
 		testString string
 		out        bool
 	}{
-		{"{}", "{}", true},
-		{"[{()}]", "[{()}]", true},
-		{"{[}]", "{[}]", false},
-		{"{{}}", "{{}}", true},
-		{"(){}", "(){}", true},
-		{"({}(", "({}(", false},
-		{"([[}", "([[)", false},
-		{")[])", ")[])", false},
-		{")", ")", false},
-		{"(", "(", false},
-		{"letters", "(1 + 2 * {3 + 4}) * [3 + 2]", true},
+		{"all bracket are balanced at center", "[{()}]", true},
+		{"wrong sequence of close brackets", "{[}]", false},
+		{"two times correct brackets", "(){}", true},
+		{"wrapped brackets are unclosed", "({}(", false},
+		{"inner brackets are unclosed", "([[)", false},
+		{"wrapped bracket are opened", ")[])", false},
+		{"one close bracket", ")", false},
+		{"one open bracket", "(", false},
+		{"true with letters", "(1 + 2 * {3 + 4}) * [3 + 2]", true},
+		{"wrong with letters", "{(1 + 2) - [3 + 4}]",false},
+		{"UTF-8 characters", "(П{Р}[ивет])", true},
+		{"empty string", "", true},
 	}
 
 	for _, tc := range testCases {
